5f6f5cc59e7dba37aa3e1b6fba5ca18c4a845449
sagarhani <sagarhani33@gmail.com>
2019-03-17 22:59:34 +0530

    Defining struct

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	// fmt.Println(alex)
 
+	var alex person
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
 }
